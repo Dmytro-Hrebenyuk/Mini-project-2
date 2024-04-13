38064ae9f5a14965f4dd70e109878a3954bfe951
Dmytro-Hrebenyuk <149362391+Dmytro-Hrebenyuk@users.noreply.github.com>
2024-04-13 18:41:32 +0300

    Update promt 1.py

--- Street Fighter 2 - Character Selection/promt 1.py
+++ Street Fighter 2 - Character Selection/promt 1.py
@@ -1,5 +1,5 @@
 # Код згенерований чатом
-def simulate_character_selection(fighters, initial_position, moves):
+def street_fighter_selection(fighters, initial_position, moves):
     rows = len(fighters)
     cols = len(fighters[0])
     current_row, current_col = initial_position
